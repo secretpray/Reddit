299b455cb0cda6d31d5bc3dade978b805def809b
secretpray <Renovatio100>
2020-10-18 20:01:25 +0300

    Add devise and create User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  FLASH_NAME = {  :success => 'alert-success', :error => 'alert-danger',
+                  :alert => 'alert-warning', :notice => 'alert-info', :safe_notice => 'alert-primary' }
+
+  def bootstrap_class_for_flash(flash_type)
+    flash = FLASH_NAME[flash_type.to_sym] || flash_type.to_s
+  end
+
+
 end
